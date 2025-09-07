2fa9c0cfbb2e6d510283fb9fd03554668cbb9288
Traian Alexandru <victor_traian@yahoo.com>
2025-09-06 22:12:57 -0400

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.scss';
 import { NavigationBar } from './components/navigation-bar/navigation-bar';
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-
   return (
-    <div className='container'>
+    <>
       <NavigationBar />
-      <div></div>
-    </div>
+      <div className='container'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint inventore
+        aut harum ratione nulla dolores ipsum, id dolor provident explicabo cum
+        illo ducimus tempore dolorem voluptatum veniam laborum suscipit
+        assumenda!
+      </div>
+    </>
   );
 }
 
