bdb2e1a44a2d26b2789c286f0c9b84e6055d8167
Traian Alexandru <victor_traian@yahoo.com>
2025-09-08 14:36:48 -0400

    add sidebar

--- src/components/navigation-bar/navigation-bar.tsx
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -2,87 +2,145 @@ import { Link } from 'react-router-dom';
 
 export const NavigationBar = () => {
   return (
-    <nav
-      className='navbar fixed-top navbar-expand-lg bg-body-tertiary'
-      data-bs-theme='dark'
-    >
-      <div className='container-fluid'>
-        <a className='navbar-brand' href='#'>
-          React
-        </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-              <Link to='/' className='nav-link active' aria-current='page'>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>
-                Link
-              </a>
-            </li>
-            <li className='nav-item dropdown'>
-              <a
-                className='nav-link dropdown-toggle'
-                href='#'
-                role='button'
-                data-bs-toggle='dropdown'
-                aria-expanded='false'
-              >
-                Dropdown
-              </a>
-              <ul className='dropdown-menu'>
-                <li>
-                  <a className='dropdown-item' href='#'>
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className='dropdown-item' href='#'>
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className='dropdown-divider' />
-                </li>
-                <li>
-                  <a className='dropdown-item' href='#'>
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link disabled' aria-disabled='true'>
-                Disabled
-              </a>
-            </li>
-          </ul>
-          <form className='d-flex' role='search'>
-            <input
-              className='form-control me-2'
-              type='search'
-              placeholder='Search'
-              aria-label='Search'
-            />
-            <button className='btn btn-outline-success' type='submit'>
-              Search
+    <>
+      <nav
+        className='navbar fixed-top navbar-expand-lg bg-body-tertiary'
+        data-bs-theme='dark'
+      >
+        <div className='container-fluid'>
+          <a
+            className='navbar-brand'
+            data-bs-toggle='offcanvas'
+            href='#offcanvasExample'
+            role='button'
+            aria-controls='offcanvasExample'
+          >
+            Components
+          </a>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+              <li className='nav-item'>
+                <Link to='/' className='nav-link active' aria-current='page'>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='#'>
+                  POCs
+                </a>
+              </li>
+              <li className='nav-item dropdown'>
+                <a
+                  className='nav-link dropdown-toggle'
+                  href='#'
+                  role='button'
+                  data-bs-toggle='dropdown'
+                  aria-expanded='false'
+                >
+                  Apps
+                </a>
+                <ul className='dropdown-menu'>
+                  <li>
+                    <a className='dropdown-item' href='#'>
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className='dropdown-item' href='#'>
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className='dropdown-divider' />
+                  </li>
+                  <li>
+                    <a className='dropdown-item' href='#'>
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link disabled' aria-disabled='true'>
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form className='d-flex' role='search'>
+              <input
+                className='form-control me-2'
+                type='search'
+                placeholder='Search'
+                aria-label='Search'
+              />
+              <button className='btn btn-outline-success' type='submit'>
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div
+        className='offcanvas offcanvas-start'
+        tab-index='-1'
+        id='offcanvasExample'
+        aria-labelledby='offcanvasExampleLabel'
+      >
+        <div className='offcanvas-header'>
+          <h5 className='offcanvas-title' id='offcanvasExampleLabel'>
+            Offcanvas
+          </h5>
+          <button
+            type='button'
+            className='btn-close'
+            data-bs-dismiss='offcanvas'
+            aria-label='Close'
+          ></button>
+        </div>
+        <div className='offcanvas-body'>
+          <div>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+          </div>
+          <div className='dropdown mt-3'>
+            <button
+              className='btn btn-secondary dropdown-toggle'
+              type='button'
+              data-bs-toggle='dropdown'
+            >
+              Dropdown button
             </button>
-          </form>
+            <ul className='dropdown-menu'>
+              <li>
+                <a className='dropdown-item' href='#'>
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className='dropdown-item' href='#'>
+                  Another action
+                </a>
+              </li>
+              <li>
+                <a className='dropdown-item' href='#'>
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </nav>
+    </>
   );
 };
