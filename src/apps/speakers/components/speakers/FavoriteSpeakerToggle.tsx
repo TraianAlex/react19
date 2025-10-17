0c885468bf653a23fc1448198eae2a52e5c8b149
Traian Alexandru <victor_traian@yahoo.com>
2025-10-17 15:37:13 -0400

    ref

--- src/apps/speakers/components/speakers/FavoriteSpeakerToggle.tsx
+++ src/apps/speakers/components/speakers/FavoriteSpeakerToggle.tsx
@@ -10,22 +10,24 @@ export default function FavoriteSpeakerToggle({
   const { updateSpeaker } = useContext(SpeakersDataContext);
   const [updating, setUpdating] = useState(false);
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const newSpeakerRec = {
+      ...speakerRec,
+      favorite: !speakerRec.favorite,
+    };
+    setUpdating(true);
+    updateSpeaker(newSpeakerRec, () => {
+      setUpdating(false);
+    });
+  };
+
   return (
     <button
       className={
         speakerRec.favorite ? 'heartredbutton btn' : 'heartdarkbutton btn'
       }
-      onClick={(e) => {
-        e.preventDefault();
-        const newSpeakerRec = {
-          ...speakerRec,
-          favorite: !speakerRec.favorite,
-        };
-        setUpdating(true);
-        updateSpeaker(newSpeakerRec, () => {
-          setUpdating(false);
-        });
-      }}
+      onClick={handleClick}
     >
       {updating ? (
         <i className='spinner-border text-dark' role='status' />
