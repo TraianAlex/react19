4d7c22ba3f0700fc578751832cb776c81c7ae323
Traian Alexandru <victor_traian@yahoo.com>
2025-11-26 17:11:49 -0500

    render prop

--- src/sandbox/compound-components/start-toggle/ToggleStarDemo.tsx
+++ src/sandbox/compound-components/start-toggle/ToggleStarDemo.tsx
@@ -1,19 +1,32 @@
 import { BsStar, BsStarFill } from 'react-icons/bs';
+import './ToggleStarDemo.modules.scss';
 
 import { Toggle } from '../Toggle/index';
 
 function ToggleStarDemo({ onChange }: { onChange: () => void }) {
   return (
-    <Toggle onToggle={onChange}>
-      <Toggle.Button>
-        <Toggle.On>
-          <BsStarFill className='star filled' />
-        </Toggle.On>
-        <Toggle.Off>
-          <BsStar className='star' />
-        </Toggle.Off>
-      </Toggle.Button>
-    </Toggle>
+    <>
+      <Toggle onToggle={onChange}>
+        <Toggle.Button>
+          <Toggle.On>
+            <BsStarFill className='star filled' />
+          </Toggle.On>
+          <Toggle.Off>
+            <BsStar className='star' />
+          </Toggle.Off>
+        </Toggle.Button>
+      </Toggle>
+
+      <Toggle>
+        <Toggle.Button>
+          <Toggle.Display>
+            {(on) => {
+              return <div className={`box ${on ? 'filled' : ''}`}></div>;
+            }}
+          </Toggle.Display>
+        </Toggle.Button>
+      </Toggle>
+    </>
   );
 }
 
